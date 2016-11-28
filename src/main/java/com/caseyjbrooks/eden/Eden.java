784cf25a150eaa86079f2d178d099cc16cdd037d
Casey Brooks <cjbrooks12@gmail.com>
2016-11-27 20:44:03 -0600

    Adds default GSON (de)serializer to Eden instance

--- src/main/java/com/caseyjbrooks/eden/Eden.java
+++ src/main/java/com/caseyjbrooks/eden/Eden.java
@@ -1,6 +1,8 @@
 package com.caseyjbrooks.eden;
 
 import com.caseyjbrooks.eden.bible.Metadata;
+import com.caseyjbrooks.eden.bible.Reference;
+import com.google.gson.GsonBuilder;
 
 public class Eden {
     private static Eden instance;
@@ -21,5 +23,20 @@ public class Eden {
 
     public Metadata getMetadata() {
         return metadata;
+    }
+
+    public GsonBuilder getSerializer() {
+        return new GsonBuilder()
+                .disableHtmlEscaping()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .registerTypeAdapter(Reference.class, new Reference.ReferenceJsonizer());
+    }
+
+    public GsonBuilder getDeserializer() {
+        return new GsonBuilder()
+                .disableHtmlEscaping()
+                .setPrettyPrinting()
+                .serializeNulls();
     }
 }
--- src/main/java/com/caseyjbrooks/eden/utils/TextUtils.java
+++ src/main/java/com/caseyjbrooks/eden/utils/TextUtils.java
@@ -1,19 +1,18 @@
 package com.caseyjbrooks.eden.utils;
 
-/**
- * Created by cbrooks on 11/27/16.
- */
 public class TextUtils {
 
     /**
-     * Returns true if the string is null or 0-length.
+     * Returns true if the string is null or empty. An empty string is defined to be either 0-length or all whitespace
      * @param str the string to be examined
-     * @return true if str is null or zero length
+     * @return true if str is null or empty
      */
     public static boolean isEmpty(CharSequence str) {
-        if (str == null || str.length() == 0)
+        if(str == null || str.length() == 0 || str.toString().trim().length() == 0) {
             return true;
-        else
+        }
+        else {
             return false;
+        }
     }
 }
